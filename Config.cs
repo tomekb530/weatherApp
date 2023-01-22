b56b719dcef16e62f85bbb8e5d16bb07797d4de8
tomekb530 <tomekb530@gmail.com>
2023-01-22 17:21:48 +0100

    Basic Config System, preparation of under the hood classes

---
+++ Config.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace weatherApp
+{
+    internal class ConfigData
+    {
+        [JsonPropertyName("apiKey")]
+        public string apiKey;
+        //add moar
+    }
+    public class Config
+    {
+        private ConfigData data;
+        public Config()
+        {
+            // Read the config file
+            string configJson = System.IO.File.ReadAllText("config.json");
+            if (configJson == null)
+            {
+                data = new ConfigData();
+            }
+            else
+            {
+                // Deserialize the config file
+                try
+                {
+                    data = JsonSerializer.Deserialize<ConfigData>(configJson);
+                }catch(Exception e)
+                {
+                    Console.WriteLine("Error reading config file: " + e.Message);
+                    data = new ConfigData();
+                }
+            }
+
+        }
+        
+        public void saveConfig()
+        {
+            string json = JsonSerializer.Serialize(data);
+            System.IO.File.WriteAllText("config.json", json);
+        }
+    }
+}
